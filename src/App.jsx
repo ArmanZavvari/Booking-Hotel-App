b18b917e4974d95a7d8bda1e1a0ea154addd6724
Arman Zavvari <armanzavvari@gmail.com>
2024-05-18 14:12:18 +0330

    feat: add LocationList Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./component/Header/Header";
+import LocationList from "./component/LocationList/LocationList";
 
 function App() {
   return (
     <div>
+      <Toaster />
       <Header />
+      <LocationList />
     </div>
   );
 }
