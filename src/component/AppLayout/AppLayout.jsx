eae075beaf3ecebae75c360338cd508a19704445
Arman Zavvari <armanzavvari@gmail.com>
2024-05-19 15:15:39 +0330

    feat: Set Map

--- src/component/AppLayout/AppLayout.jsx
+++ src/component/AppLayout/AppLayout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Map from "../Map/Map";
+import { useHotels } from "../context/HotelsProvider";
 
 function AppLayout() {
+  const { hotels } = useHotels();
+
   return (
     <div className="appLayout">
       <div className="sidebar">
         <Outlet />
       </div>
-      <div className="mapContainer">map</div>
+      <Map markerLocations={hotels} />
     </div>
   );
 }
